7fe4dfd9b75a01f82e1407d9364052bab8dc6de8
Caleb B. Gardner <ca.bosticgardner@my.ccsu.edu>
2016-10-13 12:15:31 -0400

    Updated Docs

--- Documentation/js/page-funcs.js
+++ Documentation/js/page-funcs.js
@@ -2,12 +2,4 @@ $(document).ready(function() {
     $("#button1").click(function() {
       $("#site-navigation").toggleClass("main-navigation main-navigation toggled");
     });
-
-    $("li").on("click", function(event){
-      console.log('clicked');
-    });
-
-    // $("sub-menu").on("click", function(e) {  // See here, i have our selector set to "li", so this jQuery object will grab all li tags on the page
-    //     $(this).addClass("current-menu-item").siblings().removeClass("current-menu-item");
-    // });
 });
