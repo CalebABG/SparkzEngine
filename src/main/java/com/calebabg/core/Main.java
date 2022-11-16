072e499257be27118f6bac7906d50eca9447237f
Caleb A. Bostic-Gardner <13528490+CalebABG@users.noreply.github.com>
2022-11-16 00:20:00 -0500

    Revert setting Java system property enabling OpenGL (some UI sluggishness)

--- src/main/java/com/calebabg/core/Main.java
+++ src/main/java/com/calebabg/core/Main.java
@@ -5,7 +5,6 @@ import com.calebabg.gui.StatsPanel;
 
 public class Main {
     static {
-        System.setProperty("sun.java2d.opengl", "True");
         System.setProperty("sun.java2d.transaccel", "True");
         System.setProperty("sun.java2d.ddforcevram", "True");
         System.setProperty("apple.laf.useScreenMenuBar", "True");
